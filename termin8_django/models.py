7db498dee13b6cc6ba5f87c98f34e0b30d7e2683
simenbkr <simen@haitech.org>
2017-04-21 13:16:38 +0200

    Jens did something maybe.

--- termin8_django/models.py
+++ termin8_django/models.py
@@ -50,7 +50,7 @@ class Plant(models.Model):
 #    min_temp = models.FloatField()
 #    min_moisture = models.FloatField()
 #    max_moisture = models.FloatField()
-    last_watered = models.TimeField(null=True)
+    last_watered = models.DateTimeField(null=True, auto_now_add=True)
     automatic_water = models.BooleanField()
     room = models.ForeignKey(Room)
     plant_type = models.ForeignKey(PlantType)
