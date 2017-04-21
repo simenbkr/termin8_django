dcf8b354746ccdd38573ac951ac629e95dacc4c2
simenbkr <simen@haitech.org>
2017-04-21 14:29:26 +0200

    Remove custom createmethod

--- termin8_django/serializers.py
+++ termin8_django/serializers.py
@@ -70,5 +70,5 @@ class PlantSerializer(ModelSerializer):
             return {'temp': obj.get_watering_history()[-1].get_temp(), 'moisture': obj.get_watering_history()[-1].get_moisture()}
         return {}
 
-    def create(self, validated_data):
-        return Plant.objects.create(**validated_data)
+    #def create(self, validated_data):
+     #   return Plant.objects.create(**validated_data)
